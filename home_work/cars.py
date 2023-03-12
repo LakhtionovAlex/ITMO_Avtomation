0bb09b83d12c9a7a3ac479257b6e137fbd861794
Aleksandr_Lakhtionov <04.vassals.coffees@icloud.com>
2023-03-12 22:59:25 +0300

    add Home_Work_4

--- home_work/cars.py
+++ home_work/cars.py
@@ -12,11 +12,11 @@ class Car:
     def stop():
         print('Автомобиль заглушен')
 
-    def years(self):
-        print(self.year)
+    def years(self, new_year):
+        self.year = new_year
 
-    def car_type(self):
-        print(self.types)
+    def car_type(self, types_new):
+        self.types = types_new
 
-    def car_color(self):
-        print(self.color)
+    def car_color(self, color_new):
+        self.color = color_new
